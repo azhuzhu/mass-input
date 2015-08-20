3e0105aac0e8c3dfd4efd4bf3bd44cb2ef68a87a
Yuming Zhu <yzhu@redhat.com>
2015-08-20 18:38:17 +0800

    FIX: package.json

---
+++ dist/jquery.mass-input.js
@@ -0,0 +1,33 @@
+/*! Mass Input - v0.1.0 - 2015-08-20
+* http://github.com/azhuzhu/mass-input
+* Copyright (c) 2015 Yuming Zhu; Licensed , ,  */
+(function($) {
+
+  // Collection method.
+  $.fn.mass_input = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.mass_input = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.mass_input.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.mass_input.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].mass_input = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
