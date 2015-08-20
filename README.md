1e1beea8692f53b1f2f40e0185917cf373e2ee4f
Yuming Zhu <yzhu@redhat.com>
2015-08-20 18:14:48 +0800

    Initial Project

---
+++ README.md
@@ -0,0 +1,30 @@
+# Mass Input
+
+A jQuery plugin with Bootstrap for complex input field
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/azhuzhu/mass-input/master/dist/mass-input.min.js
+[max]: https://raw.github.com/azhuzhu/mass-input/master/dist/mass-input.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/mass-input.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/jquery.mass-input.js
@@ -0,0 +1,38 @@
+/*
+ * mass-input
+ * http://github.com/azhuzhu/mass-input
+ *
+ * Copyright (c) 2015 Yuming Zhu
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.mass_input = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.mass_input = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.mass_input.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.mass_input.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].mass_input = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
