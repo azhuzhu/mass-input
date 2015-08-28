259a8344745004a4e40613234b3d6843f7a633d7
Yuming Zhu <yzhu@redhat.com>
2015-08-28 18:24:28 +0800

    REV: WRAP FUNCTIONS

--- dist/jquery.mass-input.js
+++ dist/jquery.mass-input.js
@@ -1,33 +1,33 @@
 /*! Mass Input - v0.1.0 - 2015-08-20
 * http://github.com/azhuzhu/mass-input
 * Copyright (c) 2015 Yuming Zhu; Licensed , ,  */
-(function($) {
 
-  // Collection method.
-  $.fn.mass_input = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
-    });
-  };
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // For CommonJS and CommonJS-like environments where a window with jQuery
+    // is present, execute the factory with the jQuery instance from the window object
+    // For environments that do not inherently posses a window with a document
+    // (such as Node.js), expose a Mass-input-making factory as module.exports
+    // This accentuates the need for the creation of a real window or passing in a jQuery instance
+    // e.g. require("bootstrap-tokenfield")(window); or require("bootstrap-tokenfield")($);
+    module.exports = global.window && global.window.$ ?
+      factory( global.window.$ ) :
+      function( input ) {
+        if ( !input.$ && !input.fn ) {
+          throw new Error( "mass-input requires a window object with jQuery and a jQuery instance" );
+        }
+        return factory( input.$ || input );
+      };
+  } else {
+    // Browser globals
+    factory(jQuery, window);
+  }
+}(function ($, window) {
 
-  // Static method.
-  $.mass_input = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.mass_input.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
+  "use strict"; // jshint ;_;
 
-  // Static method default options.
-  $.mass_input.options = {
-    punctuation: '.'
-  };
 
-  // Custom selector.
-  $.expr[':'].mass_input = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
-
-}(jQuery));
+}));
